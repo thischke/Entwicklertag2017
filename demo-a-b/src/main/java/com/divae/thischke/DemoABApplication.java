2d7ea054ef1f72c89fcbeec2ea34cb007d5fbee0
Thorben Hischke <thorben.hischke@netpioneer.de>
2017-05-10 17:43:22 +0200

    initial spring boot app

---
+++ demo-a-b/src/main/java/com/divae/thischke/DemoABApplication.java
@@ -0,0 +1,12 @@
+package com.divae.thischke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoABApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoABApplication.class, args);
+	}
+}
